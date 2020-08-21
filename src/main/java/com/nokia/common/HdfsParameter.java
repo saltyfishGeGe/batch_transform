e2ab6c1b81ab9065a1ec70d9d447feea6436e523
relody <1442096232@qq.com>
2020-08-21 18:19:19 +0800

    Update HdfsParameter.java

--- src/main/java/com/nokia/common/HdfsParameter.java
+++ src/main/java/com/nokia/common/HdfsParameter.java
@@ -2,25 +2,25 @@ package com.nokia.common;
 
 public class HdfsParameter {
 
-    public static final String hdfs_url = "hdfs://clusterb/tmp/ODM2/";
+    public static final String hdfs_url = "hdfs://xxxxx";
 
-    public static final String hdfs_haName = "clusterb";
+    public static final String hdfs_haName = "xxxx";
 
-    public static final String hdfs_nn1 = "hdfs://gdlt-b-master02.cnbdcu.com:8020";
+    public static final String hdfs_nn1 = "hdfs://xxxxx";
 
-    public static final String hdfs_nn2 = "hdfs://gdlt-b-master04.cnbdcu.com:8020";
+    public static final String hdfs_nn2 = "hdfs://xxxxx";
 
     public static final String authentication = "kerberos";
 
-    public static final String nn_kbs_principal = "nn/_HOST@CNBDCU.COM";
+    public static final String nn_kbs_principal = "xxxM";
 
-    public static final String dn_kbs_principal = "dn/_HOST@CNBDCU.COM";
+    public static final String dn_kbs_principal = "xxxx";
 
-    public static final String jn_kbs_principal = "jn/_HOST@CNBDCU.COM";
+    public static final String jn_kbs_principal = "xxxx";
 
-    public static final String keytab_user = "storm@CNBDCU.COM";
+    public static final String keytab_user = "xxxx";
 
-    public static final String keytab_path = "/app/bighead/terrace/kdc/storm.keytab";
+    public static final String keytab_path = "xxxx";
 
 
 }
